836917c34a979648f6262d79f81e894f3ff6121c
phankhai5004 <phankhai5004@gmail.com>
2025-11-15 08:10:29 +0700

    update .sh file

--- performance/BM_MPI/BM_MatMul_MPI_Baseline.sh
+++ performance/BM_MPI/BM_MatMul_MPI_Baseline.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-basename=$(basename "$0")
+filename=BM_MatMul_MPI_Baseline
     
 mkdir -p compile_flags
 
 rm -rf build
-make compile >> compile_flags/BM_MatMul_MPI_Baseline.txt
+make compile >> compile_flags/$filename.txt
 
 for NP in $(seq 1 32); do
-    make execute EXECUTE=build/$basename.exe MPI_NP=$NP HOSTFILE=./hosts.txt
+    make execute EXECUTE=build/$filename.exe MPI_NP=$NP HOSTFILE=./hosts.txt
 done
