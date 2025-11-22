c8388a7b6c54383d8b52c67c7cd9145f783ca58f
nguyenpanda <nguyen.hatuong0107@hcmut.edu.vn>
2025-11-22 16:31:39 +0000

    khaideptrai

--- performance/deep_upload.sh
+++ performance/deep_upload.sh
@@ -3,28 +3,28 @@
 # RUN Init
 cd BM_initialize/
 ./BM_InitializeRandom.sh
-#./BM_Main.sh
+./BM_Main.sh
 cd ..
 
 # RUN ufunc
 cd BM_ufunc/
-#./BM_RawTest_Matmul.sh
-#./BM_RawTest_VecAdd.sh
+./BM_RawTest_Matmul.sh
+./BM_RawTest_VecAdd.sh
 cd ..
 
 # RUN MatAdd
 cd BM_MatAdd/
-#./BM_MatAdd/BM_MatAdd.sh
+./BM_MatAdd/BM_MatAdd.sh
 cd ..
 
 # RUN BM_Tuning_ForkJoin
 cd BM_Tuning_ForkJoin/
-#./BM_Add_Threshold.sh
-#./BM_MatMul_GoddamnnnAdd.sh
-#./BM_MatMul_SameThreshold.sh
+./BM_Add_Threshold.sh
+./BM_MatMul_GoddamnnnAdd.sh
+./BM_MatMul_SameThreshold.sh
 cd ..
 
 # RUN OMP Strassen
 cd BM_OMP_Strassen/
-# ./BM_OMP_Strassen.sh
+./BM_OMP_Strassen.sh
 cd ..
